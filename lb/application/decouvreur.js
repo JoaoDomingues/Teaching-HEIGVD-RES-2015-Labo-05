0b1b968dfe6667ebb3e360f3ad085e3f85a989f6
JoaoDomingues <joo.dominguespedrosa@heig-vd.ch>
2015-05-27 09:53:07 +0200

    Modification pour l'accès file system dans le decouvreur

--- lb/application/decouvreur.js
+++ lb/application/decouvreur.js
@@ -15,7 +15,7 @@ decouvre.bind(protocol.PROTOCOL_PORT, function() {
 // appeler quand un nouveau message apparait
 decouvre.on('message', function(msg, source) {
    // vérifie s'il connait la source
-   if(tableauFront.indexOf(source.address) == -1 && tableauBack.indexOf(source.address) == -1){
+   if(tableauFront.indexOf(source.address) == -1 || tableauBack.indexOf(source.address) == -1){
    // vérifie si c'est un front end ou back end
       if(msg == "frontend"){
          tableauFront.push(source.address);
@@ -31,38 +31,35 @@ function misaAJourConf(){
    var that=this;
 	misaAJourConf.prototype.maj=function(){
       var i=0;
-      var filesystem=new ActiveXObject("Scripting.FileSystemObject");
+      var filesystem=require('fs');
       
-      filesystem.CreateTextFile("/usr/local/apache2/conf/extra/httpd-vhosts.conf",true);
+      var debut=filesystem.readFileSync('/app/original/my-httpd-vhosts-debut.conf', 'utf-8');
+      var milieux=filesystem.readFileSync('/app/original/my-httpd-vhosts-milieux.conf', 'utf-8');
+      var fin=filesystem.readFileSync('/app/original/my-httpd-vhosts-fin.conf', 'utf-8');
       
-      var debut=fileSystem.OpenTextFile('/app/original/my-httpd-vhosts-debut.conf', 1 ,true);
-      var milieux=fileSystem.OpenTextFile('/app/original/my-httpd-vhosts-milieux.conf', 1 ,true);
-      var fin=fileSystem.OpenTextFile('/app/original/my-httpd-vhosts-fin.conf', 1 ,true);
-      
-      var configuration=fileSystem.OpenTextFile('httpd-vhosts.conf', 8 ,true);
-      
-      // construit le début du fichier de configuration
-      configuration.WriteLine(debut.ReadAll());
+      var configuration=debut+'\n';
       
       // met les backend trouvé
-      for(i=1;i<=tableauBack.length;i++){
-         configuration.WriteLine("BalancerMember http://" + tableauBack[i-1]+":80");
+      for(i=0; i < tableauBack.length;i++){
+         configuration += "BalancerMember http://" + tableauBack[i]+ ":80" + '\n';
       }
       
-      configuration.WriteLine(milieux.ReadAll());
+      configuration += milieux;
       
       // met les front end trouvé
-      for(i=1;i<=tableauFront.length;i++){
-         configuration.WriteLine("BalancerMember http://" + tableauBack[i-1]+":80");
+      for(i=0;i < tableauFront.length;i++){
+         configuration += "BalancerMember http://" + tableauFront[i]+":80" + '\n';
       }
       
       // met la fin du fichiers
-      configuration.WriteLine(fin.ReadAll());
+      configuration += fin;
       
       // il faut ici relancer le serveur proxy pour qu'il prenne en compte
       // le nouveau fichier
       var exec = require('child_process').exec, child;
 
+      fs.writeFileSync("/usr/local/apache2/conf/extra/httpd-vhosts.conf", configuration.toString());
+      child = exec('chgrp www-data /usr/local/apache2/conf/extra/httpd-vhosts.conf')
       child = exec("/usr/local/apache2/bin/apachectl restart",
       function (error, stdout, stderr) {
          console.log('stdout: ' + stdout);
@@ -71,12 +68,11 @@ function misaAJourConf(){
             console.log('exec error: ' + error);
          }
       });
-      child();
-      
       
       // efface les tableaux en attend la prochaine mise à jour
       tabFrontEnd=[];
       tabBackEnd=[];
+      console.log("quelque chose");
 	}
    //on refait la configuration à intervalle régulier
 	setInterval(that.update,2000);
